3f7448709a0762f12642a6c94531620c1e0e6e17
koutaku0731 <koutaku731@gmail.com>
2023-11-30 09:44:42 +0900

    先にプッシュする

---
+++ MyGitProject2d/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA();
+		kadaiB();
+
+	}
+
+	//ここに課題を書く。メソッド名変えてね。
+	static void kadaiA() {
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("現在" + i + "回目のループです。");
+		}
+	}
+
+}
